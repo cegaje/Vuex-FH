d39ccef03a31febdbc33baaae2662d44a6c66c1d
cegaje <cegaje17@gmail.com>
2022-05-26 23:45:37 -0500

    mutations and actions

--- src/components/MyCounter.vue
+++ src/components/MyCounter.vue
@@ -1,8 +1,12 @@
 <template>
     <h1>Counter - Vuex</h1>
     <h2>Direct access: {{ $store.state.count }}</h2>
-    
-    <!-- <h2>Computed: {{ countComputed }}</h2> -->
+    <h2>Computed: {{ countComputed }}</h2>
+
+    <button @click="increment">+1</button>
+    <button @click="incrementBy">+5</button>
+    <button @click="incrementRandomInt">Random</button>
+
     <h1>MapState</h1>
     <h2>mapState: {{ count }}</h2>
     <h2>lastMutation: {{ lastMutation }}</h2>
@@ -21,6 +25,17 @@ export default {
         //     counter: state => state.count ,
         //     lastMutation: state => state.lastMutation
         // })
+    },
+    methods: {
+        increment() {
+            this.$store.commit('increment')
+        },
+        incrementBy() {
+            this.$store.commit('incrementBy', 5)
+        },
+        incrementRandomInt() {
+            this.$store.dispatch('incrementRandomInt')
+        }
     }
 }
 </script>
