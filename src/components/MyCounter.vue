c4ea9bc6895f2af34a81b7a73ecbb2a599b3f99a
cegaje <cegaje17@gmail.com>
2022-06-06 19:10:42 -0500

    mapActions y Vuex por modulos

--- src/components/MyCounter.vue
+++ src/components/MyCounter.vue
@@ -1,41 +1,47 @@
 <template>
     <h1>Counter - Vuex</h1>
-    <h2>Direct access: {{ $store.state.count }}</h2>
+    <h2>Direct access: {{ $store.state.counter.count }}</h2>
     <h2>Computed: {{ countComputed }}</h2>
 
     <button @click="increment">+1</button>
     <button @click="incrementBy">+5</button>
-    <button @click="incrementRandomInt">Random</button>
+    <button @click="randomIn" :disabled="isLoading">Random</button>
 
     <h1>MapState</h1>
     <h2>mapState: {{ count }}</h2>
     <h2>lastMutation: {{ lastMutation }}</h2>
+
+    <h2>Direct getter: {{ $store.getters['counter/squareCount'] }}</h2>
 </template>
 <script>
 
-import { mapState } from "vuex";
+import { mapState, mapActions } from "vuex";
 
 export default {
     computed: {
         countComputed() {
-            return this.$store.state.count;
+            return this.$store.state.counter.count;
         },
-        ...mapState(['count','lastMutation'])
+        ...mapState( 'counter' ,['count','lastMutation','isLoading'])
         // ...mapState({
         //     counter: state => state.count ,
         //     lastMutation: state => state.lastMutation
-        // })
+        // }) 
     },
     methods: {
         increment() {
-            this.$store.commit('increment')
+            this.$store.commit('counter/increment')
         },
         incrementBy() {
-            this.$store.commit('incrementBy', 5)
+            this.$store.commit('counter/incrementBy', 5)
         },
-        incrementRandomInt() {
-            this.$store.dispatch('incrementRandomInt')
-        }
+        // incrementRandomInt() {
+        //     this.$store.dispatch('incrementRandomInt')
+        // }
+        //...mapActions( 'counter', ['incrementRandomInt'])
+        ...mapActions( 'counter' , {
+            randomIn: 'incrementRandomInt'
+        })
     }
 }
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -1,31 +1,9 @@
 import { createStore } from "vuex";
-import getRandomInt  from '../helpers/getRandomInt';
+import counterStore from "./counter";
 
 export default createStore({
-    state: {
-        count: 1,
-        lastMutation: 'none',
-        isLoading: false,
-        lastRandomInt: 0
-    },
-
-    mutations: {
-        increment( state ) {
-            state.count++;
-            state.lastMutation = 'increment';
-        },
-        incrementBy( state, val ) {
-            state.count += val;
-            state.lastMutation = 'incrementBy ' + val;
-            state.lastRandomInt = val;
-        }
-    },
-
-    actions: {
-        async incrementRandomInt( context ) {
-            const randomInt = await getRandomInt();
-
-            context.commit('incrementBy', randomInt)
-        }
+    
+    modules: {
+        counter: counterStore
     }
 })
